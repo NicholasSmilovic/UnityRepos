fd3a12f2e7b40b1d6248354ec41c583594d07517
Nicholas Smilovic <nicholas_smilovic@hotmail.com>
2018-03-31 17:04:55 -0400

    started new unity project. Zombies vs plants clone

---
+++ Plants vs. Zombies/Assets/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	public AudioClip[] levelMusicChangeArray;
+	// Use this for initialization
+	void Awake () {
+		//forces game object to persist
+		DontDestroyOnLoad (gameObject);
+		Debug.Log("Don't destroy on load: " + name);
+	}
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnLevelWasLoaded (int level) {
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+		Debug.Log("Playing Clip: " +  thisLevelMusic);
+
+		if(thisLevelMusic) {//if some music attached to gameObject
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
---
+++ Plants vs. Zombies/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+	public bool autoLoad;
+
+	void Start() {
+		if(autoLoad) {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+	public void LoadLevel(string name) {
+		Debug.Log("LoadLevel: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log("quit");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel() {
+		int currentLevel = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentLevel + 1);
+	}
+}
